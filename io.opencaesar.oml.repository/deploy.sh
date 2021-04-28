e56d29f313c2a43f82f7c64dd1a0366543ea7d74
Maged Elaasar <melaasar@gmail.com>
2021-04-28 10:27:16 -0700

    change travis ci to publish p2 to github instead

--- io.opencaesar.oml.repository/deploy.sh
+++ io.opencaesar.oml.repository/deploy.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Get the current path
-CURRENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo $CURRENT_PATH
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ${CURRENT_PATH}/deploy_key.enc -out ${CURRENT_PATH}/deploy_key -d
-chmod 600 ${CURRENT_PATH}/deploy_key
-eval `ssh-agent -s`
-ssh-add ${CURRENT_PATH}/deploy_key
+git config user.name "Travis CI"
+git config user.email "melaasar@gmail.com"
 
 # Now that we're all set up, we can push.
-mvn deploy -Prelease-composite
+mvn deploy -Prelease-composite -Dgithub-update-repo=https://x-access-token:${GITHUB_API_KEY}@github.com/opencaesar/oml-p2
