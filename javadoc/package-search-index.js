ce94de8c29f8e46292eae82b45e6376d15a4372f
Travis CI <melaasar@gmail.com>
2022-06-20 08:37:18 +0000

    Built by Travis-CI: bb5918c2f5f8cb53c79417cb20f75998b220e7c3 Adding support for omljson format

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.opencaesar.oml"},{"l":"io.opencaesar.oml.impl"},{"l":"io.opencaesar.oml.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.opencaesar.oml"},{"l":"io.opencaesar.oml.impl"},{"l":"io.opencaesar.oml.resource"},{"l":"io.opencaesar.oml.util"}]
